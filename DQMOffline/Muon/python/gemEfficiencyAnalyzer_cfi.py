eb825b6cb86dcba7662432c6cd33c841124e7992
Seungjin Yang <seungjin.yang@cern.ch>
2020-07-27 11:52:37 +0900

    Fix a propagator for cosmic muons

--- DQMOffline/Muon/python/gemEfficiencyAnalyzer_cfi.py
+++ DQMOffline/Muon/python/gemEfficiencyAnalyzer_cfi.py
@@ -30,15 +30,27 @@ gemOfflineDQMCosmicMuons = cms.EDFilter("MuonSelector",
     src = cms.InputTag('muonsFromCosmics'),
     cut = cms.string(
         'outerTrack.isNonnull'
+        '&& (outerTrack.hitPattern.cscStationsWithValidHits > 0)'
     ),
     filter = cms.bool(False)
 )
+
+gemOfflineDQMCosmicMuons1Leg = cms.EDFilter("MuonSelector",
+    src = cms.InputTag('muonsFromCosmics1Leg'),
+    cut = cms.string(
+        'outerTrack.isNonnull'
+        '&& (outerTrack.hitPattern.cscStationsWithValidHits > 0)'
+    ),
+    filter = cms.bool(False)
+)
+
 
 
 gemEfficiencyAnalyzerTightGlb = DQMEDAnalyzer('GEMEfficiencyAnalyzer',
     MuonServiceProxy,
     muonTag = cms.InputTag('gemOfflineDQMTightGlbMuons'),
     recHitTag = cms.InputTag('gemRecHits'),
+    propagatorName = cms.untracked.string("SteppingHelixPropagatorAny"),
     residualXCut = cms.double(5.0),
     ptBinning = cms.untracked.vdouble(20., 30., 40., 50., 60., 70., 80., 90., 100., 120., 140., 200.),
     etaNbins = cms.untracked.int32(7),
@@ -56,13 +68,13 @@ gemEfficiencyAnalyzerSta.useGlobalMuon = cms.untracked.bool(False)
 gemEfficiencyAnalyzerSta.folder = cms.untracked.string('GEM/GEMEfficiency/StandaloneMuon')
 gemEfficiencyAnalyzerSta.logCategory = cms.untracked.string('GEMEfficiencyAnalyzerSta')
 
-
-gemEfficiencyAnalyzerCosmic = gemEfficiencyAnalyzerTightGlb.clone()
+gemEfficiencyAnalyzerCosmic = gemEfficiencyAnalyzerSta.clone()
 gemEfficiencyAnalyzerCosmic.muonTag = cms.InputTag("gemOfflineDQMCosmicMuons")
-gemEfficiencyAnalyzerCosmic.useGlobalMuon = cms.untracked.bool(False)
+gemEfficiencyAnalyzerCosmic.propagatorName = cms.untracked.string("SteppingHelixPropagatorAlong")
 gemEfficiencyAnalyzerCosmic.ptBinning = cms.untracked.vdouble(0., 10., 20., 30., 40., 50., 60., 70., 80., 90., 100., 120., 140., 200.)
 gemEfficiencyAnalyzerCosmic.folder = cms.untracked.string('GEM/GEMEfficiency/CosmicMuon')
 gemEfficiencyAnalyzerCosmic.logCategory = cms.untracked.string('GEMEfficiencyAnalyzerCosmic')
+gemEfficiencyAnalyzerCosmic.residualXCut = cms.double(20.0)
 
 
 from Configuration.Eras.Modifier_phase2_GEM_cff import phase2_GEM
